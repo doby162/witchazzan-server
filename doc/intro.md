06fda9ed0476ac27d8ddb73f87f236269d9a265d
Michael Dorian <doby162@gmail.com>
2019-09-28 17:41:28 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to witchazzan
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/witchazzan/core_test.clj
@@ -0,0 +1,7 @@
+(ns witchazzan.core-test
+  (:require [clojure.test :refer :all]
+            [witchazzan.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
