ff196628fae21c5c069a627d70bab692478089e2
Michael Dorian <doby162@gmail.com>
2019-10-02 20:20:36 -0500

    include http-kit, put config file infrastructure in place, autostart http server on port 8080

--- src/witchazzan/core.clj
+++ src/witchazzan/core.clj
@@ -5,3 +5,13 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+
+(load-file "config/config.clj")
+(require '[org.httpkit.server :as server])
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+(server/run-server app {:port (:port settings)})
