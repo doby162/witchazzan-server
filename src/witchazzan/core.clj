1acb93ef0a12984ab7076d10110a23bab9c0e30b
Michael Dorian <doby162@gmail.com>
2019-10-03 21:55:26 -0500

    functioning sockets

--- src/witchazzan/core.clj
+++ src/witchazzan/core.clj
@@ -7,11 +7,18 @@
   (println "Hello, World!"))
 
 
-
 (load-file "config/config.clj")
 (require '[org.httpkit.server :as server])
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
-(server/run-server app {:port (:port settings)})
+
+;(defn app [req]
+;  {:status  200
+;   :headers {"Content-Type" "text/html"}
+;   :body    "hello HTTP!"})
+;(server/run-server app {:port (:port settings)})
+
+(defn handler [request]
+  (server/with-channel request channel
+    (server/on-close channel (fn [status] (println "channel closed: " status)))
+    (server/on-receive channel (fn [data] ;; echo it back
+                          (server/send! channel data)))))
+(server/run-server handler {:port (:port settings)})
