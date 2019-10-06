d34582353d41e9cbe5c4e3039af1ec062733446c
Michael Dorian <doby162@gmail.com>
2019-10-05 19:15:17 -0500

    refactor and add broadcast helper

--- src/witchazzan/core.clj
+++ src/witchazzan/core.clj
@@ -1,27 +1,31 @@
+;;namespace
 (ns witchazzan.core
   (:require [clojure.data.json :as json])
+  (:require [org.httpkit.server :as server])
   (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
-
+;;namespace
+;;
+;;configuration and global state
 (load-file "config/config.clj")
-(require '[org.httpkit.server :as server])
-
-;(defn app [req]
-;  {:status  200
-;   :headers {"Content-Type" "text/html"}
-;   :body    "hello HTTP!"})
-;(server/run-server app {:port (:port settings)})
-
+(def channels [])
+(def map1 (json/read-str (slurp (str (:tilemap-path settings) (first (:tilemaps settings))))))
+;;configuration and global state
+;;
+;;websocket infrastructure
 (defn handler [request]
+  (println "A new player has entered Witchazzan")
+  (println request)
   (server/with-channel request channel
-    (server/on-close channel (fn [status] (println "channel closed: " status)))
+    (def channels (conj channels channel)); add this to our collection of channels
+    (server/on-close channel (fn [status]
+                               #_(def channels (filter (map #(= % channel) channels)))
+                               (println "channel closed: " status)))
     (server/on-receive channel (fn [data] ;; echo it back
                           (server/send! channel data)))))
+
+(defn broadcast [channels data]
+  (map (fn [channel] (server/send! channel data)) channels))
+
 (server/run-server handler {:port (:port settings)})
 
-(def map1 (json/read-str (slurp (str (:tilemap-path settings) (first (:tilemaps settings))))))
+;;websocket infrastructure
