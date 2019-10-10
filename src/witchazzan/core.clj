1b2742fab754082404e7308f207f8ad6848d7a5e
Michael Dorian <doby162@gmail.com>
2019-10-10 17:43:29 -0500

    define players as a stateful collection

--- src/witchazzan/core.clj
+++ src/witchazzan/core.clj
@@ -7,25 +7,33 @@
 ;;
 ;;configuration and global state
 (load-file "config/config.clj")
-(def channels [])
+(def players []) ; should this also be an atom?
 (def map1 (json/read-str (slurp (str (:tilemap-path settings) (first (:tilemaps settings))))))
 ;;configuration and global state
 ;;
 ;;websocket infrastructure
+(defn make-player [x y sock] (atom {:x x :y y :sock  sock}));definition of a player
+
 (defn handler [request]
   (println "A new player has entered Witchazzan")
   (println request)
   (server/with-channel request channel
-    (def channels (conj channels channel)); add this to our collection of channels
+    (def players (conj players (make-player 0 0 channel))); add this to our collection of players
     (server/on-close channel (fn [status]
-                               (def channels (filter #(not (= % channel)) channels))
+                               (def players (filter #(not (= (:sock @%) channel)) players))
                                (println "channel closed: " status)))
     (server/on-receive channel (fn [data] ;; echo it back
                           (server/send! channel data)))))
 
-(defn broadcast [channels data]
-  (map (fn [channel] (server/send! channel data)) channels))
+(defn broadcast [players data]
+  (map (fn [player] (server/send! (:sock @player) data)) players))
 
 (server/run-server handler {:port (:port settings)})
-
 ;;websocket infrastructure
+;;
+
+;;create a functional object to store client state on a per instance basis
+;;specify an api of commands and document it in the readme. We need to be able to recieve 1) inputs like the keyboard 2) state updates like an x coord 3)chat broadcasts 4)nicknames and login credentials
+;;simulate movement based on keyboard, update on client state commands, try to aproximate the front end based on only input
+;;broadcast function that specifies the physical state of all game pieces, sends chats, welcomes new players, etc
+;;
