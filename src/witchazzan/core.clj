2139ea125b706cf9f94fd849bfe151dd74923be2
Michael Dorian <doby162@gmail.com>
2019-10-11 14:07:43 -0500

    basic chat working

--- src/witchazzan/core.clj
+++ src/witchazzan/core.clj
@@ -2,17 +2,26 @@
 (ns witchazzan.core
   (:require [clojure.data.json :as json])
   (:require [org.httpkit.server :as server])
+  (:require [clojure.string :as str])
   (:gen-class))
 ;;namespace
 ;;
 ;;configuration and global state
 (load-file "config/config.clj")
 (def players []) ; should this also be an atom?
+(def anon-names ["arc" "clojure" "clojurescript" "common-lisp" "pico lisp" "scheme" "chicken" "emacs lisp" "maclisp" "racket"])
+(defn get-anon-name [] (let [first-name (first anon-names) rest-names (rest anon-names)]
+                         (def anon-names rest-names)
+                         first-name))
+;some names for those who do not have logins to borrow. Probably not a permanent fixture of the game.
 (def map1 (json/read-str (slurp (str (:tilemap-path settings) (first (:tilemaps settings))))))
 ;;configuration and global state
 ;;
 ;;websocket infrastructure
-(defn make-player [x y sock] (atom {:x x :y y :sock  sock}));definition of a player
+(defn make-player [x y sock] (atom {:x x :y y :sock sock :name (get-anon-name)}));definition of a player
+
+(defn broadcast [players data]
+  (dorun (map (fn [player] (server/send! (:sock @player) data)) players))); dorun in this context is bad form but I want to get this commit out
 
 (defn handler [request]
   (println "A new player has entered Witchazzan")
@@ -23,11 +32,12 @@
                                (def players (filter #(not (= (:sock @%) channel)) players))
                                (println "channel closed: " status)))
     (server/on-receive channel (fn [data] ;; echo it back
-                          (server/send! channel data)))))
-
-(defn broadcast [players data]
-  (map (fn [player] (server/send! (:sock @player) data)) players))
-
+                          ;(server/send! channel data)
+                          (when (re-find #"^msg," data)
+                            (broadcast players
+                                       (str "chat,"
+                                            (:name @(first (filter #(= (:sock @%) channel) players)))
+                                            ": " (last (str/split data  #"msg,")))))))))
 (server/run-server handler {:port (:port settings)})
 ;;websocket infrastructure
 ;;
