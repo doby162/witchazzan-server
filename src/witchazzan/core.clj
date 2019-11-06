3a19459757563d6d79e2124dff500dde96f988df
Michael Dorian <doby162@gmail.com>
2019-11-06 07:20:03 -0600

    refactor: reduce max line width to below 120

--- src/witchazzan/core.clj
+++ src/witchazzan/core.clj
@@ -16,7 +16,8 @@
     @id))
 
 (defn process-map
-  "returns an immutable representation of a single tilemap, inclusing a helper for collisions"
+  "returns an immutable representation of a single tilemap, inclusing a helper
+  for collisions"
   [map name]
   (let [width (get map "width") height (get map "height")
         syri (get (first (filter #(= (get % "name") "Stuff You Run Into") (get map "layers"))) "data")]
@@ -40,11 +41,16 @@
 ;;configuration and global state
 ;;
 ;;websocket infrastructure
-(defn make-player [x y sock scene direction]
-  (atom {:x x :y y :sock sock :name "unknown-human" :keys {} :id (gen-id) :scene scene :direction direction}));definition of a player
-(defn make-object [x y type scene behavior attributes]
-  (atom {:x x :y y :type type :scene scene :id (gen-id) :behavior behavior :attributes attributes}));definition of an object.
-; Objects are very general, but they will all have some things in common
+(defn make-player
+  "defines the structure of a player object"
+  [x y sock scene direction]
+  (atom {:x x :y y :sock sock :name "unknown-human" :keys {} :id (gen-id)
+         :scene scene :direction direction}))
+(defn make-object
+  "defines the structure of a game piece"
+  [x y type scene behavior attributes]
+  (atom {:x x :y y :type type :scene scene :id (gen-id) :behavior behavior
+         :attributes attributes}))
 
 (defn message-player [data player]
   (server/send! (:sock @player) (json/write-str data)))
@@ -54,16 +60,21 @@
   [data]
   (dorun (map #(message-player data %) players)))
 
-(defn establish-identity [player]
-  (message-player {:messageType "identity" :id (:id @player) :name (:name @player)} player))
+(defn establish-identity
+  "comunicates to a client which player object belongs to them"
+  [player]
+  (message-player {:messageType "identity" :id (:id @player)
+                   :name (:name @player)} player))
 
 (defn handle-chat
   "broadcasts chats as json"
   [player message]
-  (broadcast  {:messageType "chat" :name (:name @player) :id (:id @player) :content (get message "text")}))
+  (broadcast  {:messageType "chat" :name (:name @player) :id (:id @player)
+               :content (get message "text")}))
 
 (defn handle-location-update [player message]
-  (let [new-x (get message "x") new-y (get message "y") new-scene (get message "scene") new-direction (get message "direction")]
+  (let [new-x (get message "x") new-y (get message "y")
+        new-scene (get message "scene") new-direction (get message "direction")]
     (swap! player #(merge % {:x new-x :y new-y :scene new-scene :direction new-direction}))))
 
 (defn handle-login [player message]
@@ -72,39 +83,50 @@
     (establish-identity player)))
 
 (defn handle-keyboard-update [player message]
-  (swap! player #(merge
-                  %
-                  {:keys (merge
-                          (:keys %)
-                          {(str ":" (get message "key")) (get message "state")})})))
+  (swap! player
+         #(merge %
+                 {:keys
+                  (merge
+                   (:keys %)
+                   {(str ":" (get message "key")) (get message "state")})})))
 
 (defn handle-command
-  "this handler is a bit of a switch case inside of a switch case, it handles all of the text commands entered
+  "this handler is a bit of a switch case inside of a switch case,
+  it handles all of the text commands entered
   via the command bar on the client"
   [player message]
   (when (re-find #"^look" (get message "command"))
-    (message-player {"response" "You see a rhelm of unimaginable possibility."} player))
+    (message-player {"response"
+                     "You see a rhelm of unimaginable possibility."}
+                    player))
   (when (re-find #"^listen" (get message "command"))
-    (message-player {"response" "You hear the distant chatter of a keyboard. A developer is hard at work."} player)))
+    (message-player {"response"
+                     "You hear the distant chatter of a keyboard.
+                     A developer is hard at work."}
+                    player)))
 
 (def objects [])
 (defn handle-fireball
   "generate a fireball object and add it to the object registry"
   [player message]
-  (def objects (conj objects (make-object (:x @player) (:y @player) "fireball" (:scene @player) ;standard properties
-                                          (fn [this]
-                                            (cond
-                                              ((:collide (:attributes this)) this) nil
-                                              :else ((:move (:attributes this)) this)))
-                                          {:owner player ;attributes
-                                           :collide (fn [this]
-                                                      (not ((:get-tile-walkable (name->scene (:scene this))) (tile-location this))))
-                                           :move (cond
-                                                   (= "north" (get message "direction")) #(conj % {:y (dec (:y %))})
-                                                   (= "south" (get message "direction")) #(conj % {:y (inc (:y %))})
-                                                   (= "east" (get message "direction")) #(conj % {:x (inc (:x %))})
-                                                   :else #(conj % {:x (dec (:x %))})) ; west is default because we need a default
-                                           :collide-wall #(not true)}))))
+  (def objects (conj
+                objects
+                (make-object (:x @player) (:y @player) "fireball" (:scene @player) ;standard properties
+                             (fn [this]
+                               (cond
+                                 ((:collide (:attributes this)) this) nil
+                                 :else ((:move (:attributes this)) this)))
+                             {:owner player ;attributes
+                              :collide (fn [this]
+                                         (not
+                                          ((:get-tile-walkable
+                                            (name->scene (:scene this))) (tile-location this))))
+                              :move (cond
+                                      (= "north" (get message "direction")) #(conj % {:y (dec (:y %))})
+                                      (= "south" (get message "direction")) #(conj % {:y (inc (:y %))})
+                                      (= "east" (get message "direction")) #(conj % {:x (inc (:x %))})
+                                      :else #(conj % {:x (dec (:x %))}))
+                              :collide-wall #(not true)}))))
 
 (defn call-func-by-string
   "(call-func-by-string \"+\" [5 5]) => 10"
@@ -138,8 +160,17 @@
 ;;
 ;;game loop
 (defn update-clients []
-  (when (< 0 (count objects)) (broadcast {:messageType "object-state" :objects (map (fn [q] {:id (:id @q) :x (:x @q) :y (:y @q) :type (:type @q)}) objects)}))
-  (when (< 0 (count players)) (broadcast {:messageType "player-state" :players (map (fn [q] {:id (:id @q) :x (:x @q) :y (:y @q) :name (:name @q) :scene (:scene @q) :direction (:direction @q)}) players)})))
+  (when (< 0 (count objects))
+    (broadcast
+     {:messageType "object-state" :objects
+      (map (fn [q] {:id (:id @q) :x (:x @q) :y (:y @q) :type (:type @q)})
+           objects)}))
+  (when (< 0 (count players))
+    (broadcast
+     {:messageType "player-state" :players
+      (map (fn [q] {:id (:id @q) :x (:x @q) :y (:y @q) :name (:name @q)
+                    :scene (:scene @q) :direction (:direction @q)})
+           players)})))
 
 (defn process-object-behavior []
   (def objects (remove #(nil? @%) objects))
@@ -148,7 +179,6 @@
 (defn game-loop []
   (loop []
     (let [start-ms (System/currentTimeMillis)]
-        ;(def objects (filter (fn [object] @object) objects))
       (update-clients)
       (process-object-behavior)
       (Thread/sleep (- (:frame-time settings) (- (System/currentTimeMillis) start-ms))))
