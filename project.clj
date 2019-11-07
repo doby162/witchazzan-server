43b39823f608397e17575926fa863d37a3dd40c1
Michael Dorian <doby162@gmail.com>
2019-11-07 08:06:43 -0600

    Add cloverage and bikeshed with custom line length parameter
    add some probably not great unit tests to test out cloverage
    add some git hooks tht also probably need some work,
    but at least they run!

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-cljfmt "0.6.4"][jonase/eastwood "0.3.5"]]
+  :plugins [[lein-cloverage "1.1.2"][lein-bikeshed "0.5.2"][lein-cljfmt "0.6.4"][jonase/eastwood "0.3.5"]]
+  :bikeshed {:max-line-length 120}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cljfmt "0.5.1"]
                  [http-kit "2.3.0"]
--- test/witchazzan/core_test.clj
+++ test/witchazzan/core_test.clj
@@ -1,7 +1,21 @@
 (ns witchazzan.core-test
   (:require [clojure.test :refer :all]
-            [witchazzan.core :refer :all]))
+            [witchazzan.core :refer :all]
+            [clojure.string :as str]))
 
 (deftest a-test
   (testing "Tests should pass. That is how they work."
     (is (= 1 1))))
+
+(deftest check-name->scene []
+  (is (name->scene (str/replace (first (:tilemaps settings)) #".json" ""))))
+
+(deftest check-settings
+  "makes sure the settings file being loaded has reasonable values"
+  []
+  (is (integer? (:port settings)))
+  (is (contains?  settings :tilemap-path))
+  (is (contains?  settings :tilemaps))
+  (is (slurp (str (:tilemap-path settings) (first (:tilemaps settings)))))
+  (is (integer? (:frame-time settings)))
+  (is (contains? settings :pause)))
