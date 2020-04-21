f6db16af2a67c3f0f16b6c4ade624f1667637bb3
Michael Dorian <doby162@gmail.com>
2020-04-21 14:45:18 -0500

    update to latest cljfmt

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://witchazzan.space"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :plugins [[lein-cloverage "1.1.2"] [lein-bikeshed "0.5.2"] [lein-cljfmt "0.6.6"] [jonase/eastwood "0.3.5"]]
+  :plugins [[lein-cloverage "1.1.2"] [lein-bikeshed "0.5.2"] [lein-cljfmt "0.6.7"] [jonase/eastwood "0.3.5"]]
   :bikeshed {:max-line-length 120}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.clojure-goes-fast/clj-async-profiler "0.4.0"]
